f6f71b66a05ac11eaf7e1114188a9f099dc6b531
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-12 21:38:37 -0300

    diversas alterações

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './component/home-component/home-component';
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+
+export const routes: Routes = [
+    {
+        path: 'home',
+        component:HomeComponent
+    },
+
+    {
+        path:'cadastroatleta',
+        component:AtletaComponent
+    }
+
+];
