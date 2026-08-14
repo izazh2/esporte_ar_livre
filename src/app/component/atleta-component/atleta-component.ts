1a2664d02b7db975526d3c2329b1ca62b5e731e9
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-13 21:38:49 -0300

    diversas alterações

--- src/app/component/atleta-component/atleta-component.ts
+++ src/app/component/atleta-component/atleta-component.ts
@@ -1,9 +1,59 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AtletaService } from '../../service/atleta-service';
+import { Pessoa } from '../../models/pessoa';
 
 @Component({
   selector: 'app-atleta-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta-component.html',
   styleUrl: './atleta-component.css',
 })
-export class AtletaComponent {}
+export class AtletaComponent {
+
+   //DECLARAÇÃO DOS ATRIBUTOS DO COMPONENTE
+   nome = ''
+   cpf = 0
+   sexo = ''
+   cep = 0
+   ruaLogradouro = ''
+   bairro = ''
+   cidade = ''
+   uf = ''
+
+   //DECLARAÇÃO DO CONSTRUTOR
+   constructor(private atletaService: AtletaService){}
+   
+ 
+   //DECLARAÇÃO DE FUNÇÕES
+   exibeDados(){
+    console.log(this.nome, this.cpf, this.sexo, this.cep, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+   }
+
+   salvarAtleta(){
+    const pessoaAtleta = new Pessoa()
+    pessoaAtleta.nome = this.nome
+    pessoaAtleta.cpf = this.cpf 
+    pessoaAtleta.sexo = this.sexo
+    pessoaAtleta.cep = this.cep
+    pessoaAtleta.ruaLogradouro = this.ruaLogradouro
+    pessoaAtleta.bairro = this.bairro
+    pessoaAtleta.cidade = this.cidade
+    pessoaAtleta.uf = this.uf
+    
+    this.atletaService.adicionar(pessoaAtleta)
+
+    this.atletaService.listar()
+   }
+
+   limparAtributos(){
+    this.nome = ''
+    this.cpf = 0
+    this.sexo = ''
+    this.cep = 0
+    this.ruaLogradouro = ''
+    this.bairro = ''
+    this.cidade = ''
+    this.uf = ''
+   }
+}
