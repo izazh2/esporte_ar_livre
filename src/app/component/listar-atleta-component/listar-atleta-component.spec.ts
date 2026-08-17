4de9ecf51e481ba9224e7134b8b6cfa658e88bf3
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-17 19:14:51 -0300

    criado a pasta listar-atleta-component

---
+++ src/app/component/listar-atleta-component/listar-atleta-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListarAtletaComponent } from './listar-atleta-component';
+
+describe('ListarAtletaComponent', () => {
+  let component: ListarAtletaComponent;
+  let fixture: ComponentFixture<ListarAtletaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ListarAtletaComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ListarAtletaComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/component/listar-atleta-component/listar-atleta-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-atleta-component',
+  imports: [],
+  templateUrl: './listar-atleta-component.html',
+  styleUrl: './listar-atleta-component.css',
+})
+export class ListarAtletaComponent {}
