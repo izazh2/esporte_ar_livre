d27e2acb2b432f6119b9cb746371b3d3242d57e2
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-17 19:15:38 -0300

    criado a pasta listar-corrida-component

---
+++ src/app/component/listar-corrida-component/listar-corrida-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-corrida-component',
+  imports: [],
+  templateUrl: './listar-corrida-component.html',
+  styleUrl: './listar-corrida-component.css',
+})
+export class ListarCorridaComponent {}
