5a725c6f8ca27b71bcc62a9b033a7635287d296d
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-20 19:16:08 -0300

    diversas alterações

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,12 +5,12 @@ import { MenuComponent } from './component/menu-component/menu-component';
 import { AtletaComponent } from './component/atleta-component/atleta-component';
 import { HomeComponent } from './component/home-component/home-component';
 import { CorridaComponent } from './component/corrida-component/corrida-component';
-import { ListarCorridaComponent } from './component/listar-corrida-component/listar-corrida-component';
 import { AtletaListaComponent } from './component/listar-atleta-component/listar-atleta-component';
+import { CorridaListaComponent } from './component/listar-corrida-component/listar-corrida-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent, AtletaComponent, HomeComponent, CorridaComponent, AtletaListaComponent, ListarCorridaComponent],
+  imports: [RouterOutlet, MenuComponent, AtletaComponent, HomeComponent, AtletaListaComponent,CorridaComponent, CorridaListaComponent ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
