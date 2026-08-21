675c4dca03ae0e994aac97249ec622199d139ecb
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-21 20:45:01 -0300

    criado o arquivo inscricao.ts

--- src/app/component/inscricao-atleta-component/inscricao-atleta-component.ts
+++ src/app/component/inscricao-atleta-component/inscricao-atleta-component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-inscricao-atleta-component',
   imports: [],
   templateUrl: './inscricao-atleta-component.html',
   styleUrl: './inscricao-atleta-component.css',
 })
-export class InscricaoAtletaComponent {}
+export class InscricaoAtletaComponent {
+
+}
