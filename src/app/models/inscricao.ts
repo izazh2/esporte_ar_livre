d76553eeceae0fdaa99d80954a080bb354fa3af1
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-21 20:44:53 -0300

    criado o arquivo inscricao.ts

---
+++ src/app/models/inscricao.ts
@@ -0,0 +1,8 @@
+export class Inscricao{
+    atletaCadastrado: string = ''
+    corridaEscolhida: string = ''
+    distanciaProva: string = ''
+    kitTamanho: string = ''
+    categoriaFaixaetaria: string = ''
+    termosServicos: boolean = false
+}
