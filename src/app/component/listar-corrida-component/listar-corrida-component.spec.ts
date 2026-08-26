c82cb36f5194eb2c9a3fb28ec59e742a36e0efc7
Isabelly Cristina <isabelly.c.sales@aluno.senai.br>
2026-08-26 19:05:58 -0300

    diversas alterações

--- src/app/component/listar-corrida-component/listar-corrida-component.spec.ts
+++ src/app/component/listar-corrida-component/listar-corrida-component.spec.ts
@@ -1,22 +1,53 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ListarCorridaComponent } from './listar-corrida-component';
+import { Corrida } from '../../models/corrida';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
+
+import { provideHttpClient } from '@angular/common/http';
+import { CorridaService } from '../../service/corrida/corrida-service';
+import { AtletaService } from '../../service/atleta-service';
 
 describe('ListarCorridaComponent', () => {
-  let component: ListarCorridaComponent;
-  let fixture: ComponentFixture<ListarCorridaComponent>;
+
+  let service : CorridaService
+  let httpMock : HttpTestingController
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListarCorridaComponent],
+      providers: [
+        AtletaService,
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ListarCorridaComponent);
-    component = fixture.componentInstance;
-    await fixture.whenStable();
+    service = TestBed.inject(CorridaService)
+
+    httpMock = TestBed.inject(HttpTestingController)
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('Resultado esperado a lista corrida', ()=>{
+    const corridas : Corrida[] = [{
+      "descricao_corrida":"CORRIDA DOS CARAS ",
+      "data_corrida":"2067-07-06",
+      "distancia5km":true,
+      "distancia10km":true,
+      "distancia25km":false,
+      "id":12
+    }]
+
+    service.listarCorridas().subscribe(result=>{
+      expect(result).toEqual(corridas)
+    })
+
+    const requisicao = httpMock.expectOne('https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/corrida')
+
+    expect(requisicao.request.method).toBe('GET')
+
+    requisicao.flush(corridas)
+  })
+
+ 
+
 });
